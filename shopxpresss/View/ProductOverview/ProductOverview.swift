d73a6050fe37849768a04f876f9c247393ac1f7d
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-09-09 22:35:26 +0300

    Update ProductOverview.swift

--- shopxpresss/View/ProductOverview/ProductOverview.swift
+++ shopxpresss/View/ProductOverview/ProductOverview.swift
@@ -7,30 +7,84 @@
 
 import SwiftUI
 
+struct Person: Identifiable {
+   let givenName: String
+   let familyName: String
+   let emailAddress: String
+   let id = UUID()
+   var fullName: String { givenName + " " + familyName }
+}
+
 struct ProductOverview: View {
-   var product_name:String?
-   var product_id: Int?
+   @ObservedObject var homeWebS = HomeWebS()
+   @ObservedObject var favoritesWebS = FavoritesWebS()
+   var product: ProductReviewModal?
    @ObservedObject var historyWebS = HistoryWebS()
-    var body: some View {
-       ZStack{
-          Text("\(product_name ?? "non")")
-       }
-       .background(Color.white)
-       .navigationBarTitle("\(product_name ?? "")", displayMode: .inline)
-       .onAppear {
-          historyWebS.AddToHistory(product_id: product_id!)
-          let appearance = UINavigationBarAppearance()
-          appearance.backgroundColor = .white
-          appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-          UINavigationBar.appearance().standardAppearance = appearance
-          UINavigationBar.appearance().scrollEdgeAppearance = appearance
-          UINavigationBar.appearance().compactAppearance = appearance
-       }
-    }
+   @State var addToCartSheet = false
+   var body: some View {
+      GeometryReader{ geo in
+         let geoW = geo.size.width
+         let geoH = geo.size.height
+         ZStack{
+            ScrollView{
+               VStack{
+                  //Slider
+                  ProductOverviewIMageSlider(geoW: geoW, geoH: geoH)
+                  
+                  VStack(alignment: .leading, spacing: 20){
+                     
+                     //Product Data
+                     ProductOverviewData(
+                        product_id: product!.product_id!,
+                        favorite_state: product!.favorite_state ?? false,
+                        favoritesWebS: favoritesWebS
+                     )
+                     //Buttons
+                     ProductOverviewButtons(product_id: (product?.product_id)!, addToCartSheet: $addToCartSheet)
+                     
+                     //Colours
+                     ColoursOfProduct()
+                     
+                     //Seller
+                     ProductOverviewSellerData()
+                     ProductOverviewProductDetails()
+                     
+                     //Similar Products
+                     SimilarProducts(homeWebS: homeWebS, favoritesWebS: favoritesWebS)
+                  }
+                  .padding(.horizontal)
+                  Spacer()
+               }
+            }
+            Spacer()
+         }
+         .sheet(isPresented: $addToCartSheet){
+            SheetCartView(addToCartSheet: $addToCartSheet, product_name: (product?.product_name)!)
+               .presentationDetents([.height(200)])
+         }
+         .frame(width: geoW, height: geoH)
+         .background(Color.white)
+         .navigationBarTitle("\(product!.product_name ?? "")", displayMode: .inline)
+         .onAppear {
+            historyWebS.AddToHistory(product_id: (product?.product_id)!)
+            homeWebS.getProducts()
+            favoritesWebS.getFavoriteIds()
+            let pageControl = UIPageControl.appearance()
+            pageControl.pageIndicatorTintColor = UIColor.gray.withAlphaComponent(0.2)
+            pageControl.currentPageIndicatorTintColor = UIColor.black
+         }
+      }
+   }
 }
 
 struct ProductOverview_Previews: PreviewProvider {
-    static var previews: some View {
-       ProductOverview()
-    }
+   static var previews: some View {
+      ProductOverview()
+   }
 }
+
+
+
+
+
+
